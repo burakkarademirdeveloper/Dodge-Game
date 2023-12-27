7e34c9f157531a35dddf4eacc73cd293d7264f4b
burak karademir <burakkarademirmail@gmail.com>
2023-12-27 16:54:09 +0300

    add: arrow direction script

--- Assets/Scripts/Controllers/ArrowDirection.cs
+++ Assets/Scripts/Controllers/ArrowDirection.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ArrowDirection : MonoBehaviour
+namespace Controllers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ArrowDirection : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void GetDirection(GameObject character)
+        {
+            var direction = (character.transform.position - transform.position);
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 }
--- Assets/Scripts/Controllers/SpawnController.cs
+++ Assets/Scripts/Controllers/SpawnController.cs
@@ -6,9 +6,10 @@ namespace Controllers
     public class SpawnController : MonoBehaviour
     {
         [SerializeField] private List<GameObject> _spawnObjects; //There is an object now. I will add more objects later.
-        
         [SerializeField] private float _firstSpawnTime;
         [SerializeField] private float _repeatSpawnTime;
+
+        [SerializeField] private GameObject _character;
         private void Start()
         {
             InvokeRepeating(nameof(SpawnArrow), _firstSpawnTime, _repeatSpawnTime);
@@ -17,7 +18,9 @@ namespace Controllers
         private void SpawnArrow()
         {
             var randomIndex = Random.Range(0, _spawnObjects.Count);
-            Instantiate(_spawnObjects[randomIndex], transform.position, Quaternion.identity);
+            var spawnedObject = Instantiate(_spawnObjects[randomIndex], transform.position, Quaternion.identity);
+            
+            spawnedObject.GetComponent<ArrowDirection>().GetDirection(_character);
         }
     }
 }
